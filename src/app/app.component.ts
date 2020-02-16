d54889bff883ab1e1690cfc615f5a0de767dd569
Sebastián <sebtstian@gmail.com>
2020-02-16 13:42:08 -0300

    formularior reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+// import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactiveForm';
+  form: FormGroup;
+  mostrarInfo: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    // Se realiza la llamada en el constructor para una instancia inmediata
+    // Si se espera a cargar en ngOnInit causaria un error
+    this.formulario();
+  }
+
+  formulario() {
+    this.form = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+    // this.form.valueChanges.pipe(debounceTime(500)).subscribe(console.log);
+    // this.form.get('email').valueChanges.subscribe(console.log);
+  }
+
+  // Obteniendo los campos del formulario
+  get campoNombre() {
+    return this.form.get('nombre');
+  }
+
+  get campoEmail() {
+    return this.form.get('email');
+  }
+
+  guardar(form: FormGroup) {
+    if (this.form.invalid) return;
+
+    this.mostrarInfo = true;
+    console.log('Información del formulario:', form);
+  }
 }
